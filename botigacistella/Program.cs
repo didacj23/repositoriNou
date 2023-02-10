bfecf857876eb63390f44c1336befcd119e5564a
Dídac <didac@didac.com>
2023-02-10 16:23:47 +0100

    Primer commit x recuperar el codi del repositori antic

---
+++ botigacistella/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace botiga_cistella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables i taules de botiga
+            string[] productesBotiga = {};
+            decimal[] preusProductes = { };
+
+            int nElemBotiga = 0;
+            //variables i taules de botiga
+
+            //variables i taules de cistella
+            string[] productesCistella = { };
+            string[] quantitat = { };
+            string[] nElemCistella = { };
+            decimal diners = 0;
+            //variables i taules de cistella
+
+            //info: Hem de tenir en compte que per a cada producte tindrem la seva quantitat, per tant quan inserim un
+            //producte hem d’inserir una quantitat i actualitzar nElements, no podrem posar un producte a la cistella si no
+            //tenim suficient diners.
+
+
+
+
+        }
+    }
+}
